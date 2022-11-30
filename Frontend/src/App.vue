469d64437e186ed561f993c7dd6b63475dc7e336
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2022-11-30 17:56:09 +0100

    Login/logout functionality

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,18 +1,61 @@
 <template>
 
-  <Home></Home>
+  <div class="text-center text-accent my-20 text-5xl">Vue + Spotify App</div>
+
+  <div v-if="!token" class="text-center">
+
+    <button @click="greet" class="text-accent m-10">CHECK TOKEN</button>
+
+    <button class="text-accent bg-white p-5 bg-opacity-0 hover:bg-opacity-30" @click="login">
+      <a href="http://localhost:8888/login">Login with Spotify</a>
+    </button>
+
+  </div>  
+
+
+  <div v-else class="text-center">
+
+    <button @click="greet" class="text-accent m-10">CHECK TOKEN</button>
+
+    <div  class="text-accent">Logged in!</div>
+
+    <button class="text-accent bg-white p-5 bg-opacity-0 hover:bg-opacity-30" @click="logoutUser">
+      Logout
+    </button>
+
+  </div>  
 
 </template>
 
-<script lang="ts">
-import Home from './components/Home.vue'
+<script>
 
-export default{
-    name: "App",
-    components: {
-        Home
+import { accessToken, logout, getCurrentUserProfile } from './spotify.js'
+
+let token = accessToken;
+let profile = await getCurrentUserProfile;
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      token: token
     }
+  },
+  methods: {
+    greet(){
+      alert(`Token ${this.token}!`);
+    },
+    logoutUser(){
+      logout();
+    },
+  }
 }
+
+
+
+
+
+
 
 
 </script>
---
+++ Frontend/src/spotify.js
@@ -0,0 +1,119 @@
+import axios from 'axios';
+
+const LOCAL_STORAGE_KEYS = {
+    accessToken: 'spotify_access_token',
+    refreshToken: 'spotify_refresh_token',
+    expireTime: 'spotify_token_expire_time',
+    timestamp: 'spotify_token_timestamp',
+  }
+
+
+const LOCAL_STORAGE_VALUES = {
+    accessToken: window.localStorage.getItem(LOCAL_STORAGE_KEYS.accessToken),
+    refreshToken: window.localStorage.getItem(LOCAL_STORAGE_KEYS.refreshToken),
+    expireTime: window.localStorage.getItem(LOCAL_STORAGE_KEYS.expireTime),
+    timestamp: window.localStorage.getItem(LOCAL_STORAGE_KEYS.timestamp),
+}
+
+const getAccessToken = () => {
+
+    let queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+
+    const queryParams = {
+    [LOCAL_STORAGE_KEYS.accessToken]: urlParams.get('access_token'),
+    [LOCAL_STORAGE_KEYS.refreshToken]: urlParams.get('refresh_token'),
+    [LOCAL_STORAGE_KEYS.expireTime]: urlParams.get('expires_in'),
+    };
+
+    const hasError = urlParams.get('error');
+
+    if (hasError || hasTokenExpired() || LOCAL_STORAGE_VALUES.accessToken === 'undefined') {
+        refreshToken();
+    }
+
+    // If there is a valid access token in localStorage, use that
+    if (LOCAL_STORAGE_VALUES.accessToken && (LOCAL_STORAGE_VALUES.accessToken !== 'undefined')) {
+        return LOCAL_STORAGE_VALUES.accessToken;
+    }
+
+    // If there is a token in the URL query params, user is logging in for the first time
+    if (queryParams[LOCAL_STORAGE_KEYS.accessToken]) {
+
+        // Store the query params in localStorage
+        for (const property in queryParams) {
+            window.localStorage.setItem(property, queryParams[property]);
+        }
+
+        // Set timestamp
+        window.localStorage.setItem(LOCAL_STORAGE_KEYS.timestamp, Date.now());
+
+        // Return access token from query params
+        return queryParams[LOCAL_STORAGE_KEYS.accessToken];
+    }
+
+    // We should never get here!
+    return false;
+}
+
+const refreshToken = async () => {
+    try {
+        // Logout if there's no refresh token stored or we've managed to get into a reload infinite loop
+        if (!LOCAL_STORAGE_VALUES.refreshToken ||
+            LOCAL_STORAGE_VALUES.refreshToken === 'undefined' ||
+            (Date.now() - Number(LOCAL_STORAGE_VALUES.timestamp) / 1000) < 1000
+        ){
+            console.error('No refresh token available');
+            logout();
+        }
+    
+        // Use `/refresh_token` endpoint from our Node app
+        const { data } = await axios.get(`/refresh_token?refresh_token=${LOCAL_STORAGE_VALUES.refreshToken}`);
+    
+        // Update localStorage
+        window.localStorage.setItem(LOCAL_STORAGE_KEYS.accessToken, data.access_token);
+        window.localStorage.setItem(LOCAL_STORAGE_KEYS.timestamp, Date.now());
+
+        
+    
+        window.location.reload();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const hasTokenExpired = () => {
+
+    const { accessToken, timestamp, expireTime } = LOCAL_STORAGE_VALUES;
+    if (!accessToken || !timestamp) {
+      return false;
+    }
+    const millisecondsElapsed = Date.now() - Number(timestamp);
+    return (millisecondsElapsed / 1000) > Number(expireTime);
+}
+
+export const logout = () => {
+    // Clear all localStorage items
+    for (const property in LOCAL_STORAGE_KEYS) {
+        window.localStorage.removeItem(LOCAL_STORAGE_KEYS[property]);
+    }
+    // Navigate to homepage
+    window.location = window.location.origin;
+}
+
+export const accessToken = getAccessToken();
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+ axios.defaults.baseURL = 'https://api.spotify.com/v1';
+ axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+ axios.defaults.headers['Content-Type'] = 'application/json';
+
+ /**
+ * Get Current User's Profile
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+ * @returns {Promise}
+ */
+export const getCurrentUserProfile = () => axios.get('/me');
