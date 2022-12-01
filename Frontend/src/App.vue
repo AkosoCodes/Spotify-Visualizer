0a7d3b03dbc29a555b6e81a333b2da65fe3a4ea9
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2022-12-01 14:04:15 +0100

    Installed Pinia

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,73 +1,70 @@
 <template>
 
+  <Navbar/> 
+
   <div class="text-center text-accent my-20 text-5xl">Vue + Spotify App</div>
 
   <div v-if="!token" class="text-center">
-
-    <button @click="greet" class="text-accent m-10">CHECK TOKEN</button>
 
     <button class="text-accent bg-white p-5 bg-opacity-0 hover:bg-opacity-30" @click="login">
       <a href="http://localhost:8888/login">Login with Spotify</a>
     </button>
 
-  </div>  
+  </div>
 
 
   <div v-else class="text-center">
 
-    <button @click="greet" class="text-accent m-10">CHECK TOKEN</button>
+    <div class="text-accent">Logged in!</div>
 
-    <div  class="text-accent">Logged in!</div>
-
-    <button class="text-accent bg-white p-5 bg-opacity-0 hover:bg-opacity-30" @click="logoutUser">
+    <button class="text-accent bg-white p-5 bg-opacity-0 hover:bg-opacity-30" @click="store.logoutUser()">
       Logout
     </button>
 
-  </div>  
+  </div>
+
+  <Home></Home>
 
 </template>
 
 <script>
 
-import { accessToken, logout, getCurrentUserProfile } from './spotify.js'
+import Home from './components/Home.vue';
+import Navbar from './components/Navbar.vue';
+
+import { useUserStore } from './stores/UserStore.js'
+import { storeToRefs } from 'pinia'
 import { catchErrors } from './utils.js'
 
-let token = accessToken;
-let profile = await getCurrentUserProfile;
-
-
-const fetchData = async () => {
-    const {data} = await getCurrentUserProfile();
-    let profile = data;  
-}
-
-if(token){
-  catchErrors(fetchData());
-}
 
 export default {
   name: 'App',
-  data() {
+  setup() {
+
+    const store = useUserStore();
+    const { token, profile } = storeToRefs(store);
+
+    if(token){
+      catchErrors(store.fetchProfile());
+    }
+
     return {
-      token: token,
-      profile: profile
+      profile,
+      token,
+      store,
     }
   },
   methods: {
-    greet(){
-      alert(`Token ${this.token}!`);
+    logout() {
+      this.store.logoutUser();
     },
-    logoutUser(){
-      logout();
-    },
-  }
+  },
+  components: {
+    Home,
+    Navbar,
+
+  },
 }
-
-
-
-
-
-
 
 
 </script>
