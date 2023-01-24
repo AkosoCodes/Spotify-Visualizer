d9d91b318400a07e22842571f243a990b12cc092
AkosoCodes <74971935+AkosoCodes@users.noreply.github.com>
2023-01-24 13:53:15 +0100

    fix

--- Frontend/src/views/Profile.vue
+++ Frontend/src/views/Profile.vue
@@ -35,50 +35,52 @@ export default {
 </script>
 
 <template>
-  <div
-    v-if="profile !== undefined"
-    class="flex flex-col w-[250px] mx-auto mt-[50px]"
-  >
-    <img
-      v-if="profile.images !== undefined"
-      class="rounded-full shadow-circle w-[180px] mx-auto"
-      :src="profile.images[0].url"
-      alt="profile picture"
+  <div>
+    <div
+      v-if="profile !== undefined"
+      class="flex flex-col w-[250px] mx-auto mt-[50px]"
     >
-    <h1 class="text-center hover:text-accent cursor-pointer">
-      <a
-        id="bold"
-        :href="profile.href"
-      >{{ profile.display_name }}</a>
-    </h1>
-
-    <ul class="flex justify-center">
-      <li class="flex flex-col mx-5">
-        <span class="text-gray">Followers</span>
-        <span
-          v-if="profile.followers !== undefined"
-          class="text-center text-white"
-        >{{ profile.followers.total }}</span>
-      </li>
-      <li class="flex flex-col mx-5">
-        <span class="text-gray">Playlists</span>
-        <span class="text-center text-white">{{ playlists.total }}</span>
-      </li>
-    </ul>
-
-    <button
-      class="border-white border-2 w-[180px]  h-[40px] rounded-full my-5 mx-auto hover:bg-accent hover:border-accent text-[18px] text-white hover:text-white font-bold"
-      @click="logout"
-    >
-      LOGOUT
-    </button>
-
-    <div class="flex justify-around">
+      <img
+        v-if="profile.images !== undefined"
+        class="rounded-full shadow-circle w-[180px] mx-auto"
+        :src="profile.images[0].url"
+        alt="profile picture"
+      >
+      <h1 class="text-center hover:text-accent cursor-pointer">
+        <a
+          id="bold"
+          :href="profile.href"
+        >{{ profile.display_name }}</a>
+      </h1>
+    
+      <ul class="flex justify-center">
+        <li class="flex flex-col mx-5">
+          <span class="text-gray">Followers</span>
+          <span
+            v-if="profile.followers !== undefined"
+            class="text-center text-white"
+          >{{ profile.followers.total }}</span>
+        </li>
+        <li class="flex flex-col mx-5">
+          <span class="text-gray">Playlists</span>
+          <span class="text-center text-white">{{ playlists.total }}</span>
+        </li>
+      </ul>
+    
+      <button
+        class="border-white border-2 w-[180px]  h-[40px] rounded-full my-5 mx-auto hover:bg-accent hover:border-accent text-[18px] text-white hover:text-white font-bold"
+        @click="logout"
+      >
+        LOGOUT
+      </button>
+    </div>
+    
+    <div class="flex justify-around ">
       <ArtistTile
         :artists="topArtistsShort.items"
         :compact="true"
       />
-    
+      
       <TrackTile
         :tracks="topTracksShort.items"
         :compact="true"
